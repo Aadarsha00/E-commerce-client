77212f26c2e67af44f87fbd8364b8a7e31d2cfed
Aadarsha Subedi <subediaadarsha0@gmail.com>
2025-04-24 09:38:52 +0545

    change cart Isloading

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -61,7 +61,11 @@ const Index = () => {
     clearCart();
   };
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex justify-center items-center h-64">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
+      </div>
+    );
   }
   //if the cart is empty
   if (!data?.data?.items || data?.data?.items.length === 0) {
